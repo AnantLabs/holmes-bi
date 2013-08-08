01034b07f3440519297ce5192f81fad2d8d07353
Alexander Fenske <alexanderfenske@web.de>
2013-08-08 21:25:05 +0200

    Update configuration and db information

---
+++ dashboard/config.default.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = array();
+$config["datadb"] = array();
+$config["datadb"]["name"] = "TODO";
+$config["datadb"]["username"] = "TODO";
+$config["datadb"]["password"] = "TODO";
+$config["datadb"]["host"] = "TODO";
+
+$config["metadb"] = array();
+$config["metadb"]["name"] = "TODO";
+$config["metadb"]["username"] = "TODO";
+$config["metadb"]["password"] = "TODO";
+$config["metadb"]["host"] = "TODO";
+
+?>
--- dashboard/lib/dbconnection.php
+++ dashboard/lib/dbconnection.php
@@ -11,41 +11,42 @@ define("TYPE_DASHBOARD_DB", 1);
 define("TYPE_METADATA_DB", 2);
 
 class dbconnection {
-    var $db; //databasename for dashboard db
-    var $db_username; //username for dashboard db
-    var $db_password; //password for dashboard db
-    var $db_host; //host for dashboard db
-    var $md_db; //databasename for metadatadb
-    var $md_db_username; //username for metadata db
-    var $md_db_password; //password for metadata db
-    var $md_db_host; //host for metadata db
+    var $datadb; //databasename for dashboard db
+    var $datadb_username; //username for dashboard db
+    var $datadb_password; //password for dashboard db
+    var $datadb_host; //host for dashboard db
     
-    var $db_connection = null; //databaseobject for dashboard db
-    var $md_db_connection = null; //databaseobject for metadatabase db
+    var $metadb; //databasename for metadatadb
+    var $metadb_username; //username for metadata db
+    var $metadb_password; //password for metadata db
+    var $metadb_host; //host for metadata db
+    
+    var $datadb_connection = null; //databaseobject for dashboard db
+    var $metadb_connection = null; //databaseobject for metadatabase db
     
     /**
      * TODO
      */
     public function init() {
-        $this->db = $config["db"]["name"];
-        $this->db_username = $config["db"]["username"];
-        $this->db_password = $config["db"]["password"];
-        $this->db_host = $config["db"]["host"];
+        $this->datadb = $config["db"]["name"];
+        $this->datadb_username = $config["db"]["username"];
+        $this->datadb_password = $config["db"]["password"];
+        $this->datadb_host = $config["db"]["host"];
         
-        $this->mb_db = $config["mb_db"]["name"];
-        $this->mb_db_username = $config["mb_db"]["username"];
-        $this->mb_db_password = $config["mb_db"]["password"];
-        $this->mb_db_host = $config["mb_db"]["host"];
+        $this->metadb = $config["mb_db"]["name"];
+        $this->metadb_username = $config["mb_db"]["username"];
+        $this->metadb_password = $config["mb_db"]["password"];
+        $this->metadb_host = $config["mb_db"]["host"];
     }
     
     /**
      * TODO
      */
     public function start() {
-        $this->db_connection = mysql_connect($this->db_host, $this->db_username, $this->db_password) or die("Keine Verbindung möglich: " . mysql_error());
-        mysql_select_db($this->db, $this->db_connection);
-        $this->md_db_connection = mysql_connect($this->md_db_host, $this->md_db_username, $this->md_db_password) or die("Keine Verbindung möglich: " . mysql_error());
-        mysql_select_db($this->md_db, $this->md_db_connection);
+        $this->datadb_connection = mysql_connect($this->datadb_host, $this->datadb_username, $this->datadb_password) or die("Keine Verbindung möglich: " . mysql_error());
+        mysql_select_db($this->datadb, $this->datadb_connection);
+        $this->metadb_connection = mysql_connect($this->metadb_host, $this->metadb_username, $this->metadb_password) or die("Keine Verbindung möglich: " . mysql_error());
+        mysql_select_db($this->metadb, $this->metadb_connection);
     }
     
     /**
@@ -98,8 +99,8 @@ class dbconnection {
      * close all db connections
      */
     public function close() {
-        mysql_close($this->db_connection);
-        mysql_close($this->md_db_connection);
+        mysql_close($this->datadb_connection);
+        mysql_close($this->metadb_connection);
     }
 }
 ?>
